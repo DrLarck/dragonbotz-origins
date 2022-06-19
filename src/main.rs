0e4783327ae1aa2ea76f7af7a51f2b31077772f3
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-06-19 17:16:15 +0200

    Adds implementation to run the bot

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@
 mod core;
 mod utils;
 
+// lib
+    // serenity
+use serenity::client::{
+    Client,
+};
+
+use serenity::prelude::*;
+
 // core
 use crate::core::bot::Bot;
 
@@ -19,6 +27,19 @@ async fn main() {
         Config::environment_var::<u64>("DRAGONBOTZ_TEST_GUILD_ID"),
     );
 
+    let pre_client = Client::builder(bot.token(), GatewayIntents::empty())
+        .application_id(bot.application_id())
+        .await;
     
+    if let Ok(mut client) = pre_client {
+        
+        match client.start().await {
+            Ok(()) => (),
+            Err(error) => panic!("Error in main: Unable to start the client: {}", error)
+        }
+
+    } else if let Err(error) = pre_client {
+        panic!("Error in main: Error creating client: {}", error)
+    }
 
 }
