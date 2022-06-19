c8114338bbaa915db368353bcaec095a7919567d
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-06-19 19:35:10 +0200

    Adds commands to the bot

--- src/core/bot.rs
+++ src/core/bot.rs
@@ -3,6 +3,17 @@
     // std
 use std::collections::HashMap;
 
+    // serenity
+use serenity::async_trait;
+use serenity::prelude::EventHandler;
+use serenity::client::Context;
+use serenity::model::{
+    gateway::Ready,
+    id::GuildId,
+    interactions::application_command::ApplicationCommand,
+};
+
+
 // core
 use crate::core::command::Command;
 
@@ -52,6 +63,50 @@ impl Bot {
     /// Returns the bot's commands
     pub fn commands(&self) -> &HashMap<String, Box<dyn Command>> {
         &self.commands
+    }
+
+}
+
+#[async_trait]
+impl EventHandler for Bot {
+
+    async fn ready(&self, context: Context, _ready: Ready) {
+
+        // init
+        let test_guild = GuildId(self.test_guild_id());
+        let bot_commands = self.commands();
+        let mut added_commands = Vec::<ApplicationCommand>::new();
+
+        // for every command stored in the bot, create a slash command
+        let temp_added_commands = test_guild.set_application_commands(
+            &context.http, |command| {
+
+                for (_key, cmd) in bot_commands {
+                    command.create_application_command(|new_command| {
+                        new_command
+                            .name(cmd.name())
+                            .description(cmd.description())
+                    });
+                }
+
+                command
+                
+            }).await;
+        
+        if let Ok(temp) = temp_added_commands {
+            added_commands = temp;
+        } else if let Err(error) = temp_added_commands {
+            panic!("Error in Bot::ready: Error while setting up test guild slash commands: {}", error)
+        }
+
+        // becomes immutable
+        let added_commands = added_commands;
+
+        // displays all the added commands
+        for command in added_commands {
+            println!("Command {:?} had been added", command);
+        }
+
     }
 
 }
