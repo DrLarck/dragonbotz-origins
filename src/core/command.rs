d00e7148bc6778899992646f2ae6ebf728aa9665
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-06-19 18:40:11 +0200

    Fixes Trait declaration

--- src/core/command.rs
+++ src/core/command.rs
@@ -8,16 +8,16 @@ use serenity::builder::CreateEmbed;
 #[async_trait]
 pub trait Command: Sync + Send {
 
-    /// Returns the command's content
-    async fn content(&self) -> Option<String>;
-
-    /// Returns the command's embed
-    async fn embed(&self) -> Option<CreateEmbed>;
-
     /// Returns the command's name
-    fn name(&self) -> String;
+    fn name(self: &Self) -> String; 
 
     /// Returns the command's description
-    fn description(&self) -> String;
+    fn description(self: &Self) -> String;
+
+    /// Returns the command's content
+    async fn content(self: &Self) -> Option<String> { None }
+
+    /// Returns the command's embed
+    async fn embed(self: &Self) -> Option<CreateEmbed> { None }
 
 }
