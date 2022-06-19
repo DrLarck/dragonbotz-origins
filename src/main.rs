daec285aea61cc243c6f1006ee735c95c98753e9
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-06-19 18:40:42 +0200

    Adds a way to fetch and store commands

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 
 // mods
+mod commands;
 mod core;
 mod utils;
 
 // lib
+    // std
+use std::collections::HashMap;
+
     // serenity
 use serenity::client::{
     Client,
@@ -11,8 +15,12 @@ use serenity::client::{
 
 use serenity::prelude::*;
 
+// commands
+use crate::commands::ping::PingCommand;
+
 // core
 use crate::core::bot::Bot;
+use crate::core::command::Command;
 
 // utils
 use crate::utils::config::Config;
@@ -20,11 +28,20 @@ use crate::utils::config::Config;
 
 #[tokio::main]
 async fn main() {
+
+    let mut commands = HashMap::<String, Box<dyn Command>>::new();
+
+    // inserts commands into commands hashmap
+    let commands_array: [Box<dyn Command>; 1] = [Box::new(PingCommand)];
+    for command in commands_array {
+        commands.insert(command.name(), command);
+    }
     
     let bot = Bot::new(
         Config::environment_var::<String>("DRAGONBOTZ_TOKEN"),
         Config::environment_var::<u64>("DRAGONBOTZ_APP_ID"),
         Config::environment_var::<u64>("DRAGONBOTZ_TEST_GUILD_ID"),
+        commands,
     );
 
     let pre_client = Client::builder(bot.token(), GatewayIntents::empty())
