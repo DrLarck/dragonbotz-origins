a9f42bb78d75adf14b20f2c7af489fba47385d46
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-06-19 18:40:54 +0200

    Adds Ping command

---
+++ src/commands/ping.rs
@@ -0,0 +1,23 @@
+
+// lib
+    // serenity
+use serenity::async_trait;
+
+// core
+use crate::core::command::Command;
+
+
+pub struct PingCommand;
+
+#[async_trait]
+impl Command for PingCommand {
+
+    fn name(&self) -> String {
+        "ping".to_string()
+    }
+
+    fn description(&self) -> String {
+        "A simple ping command.".to_string()
+    }
+
+}
