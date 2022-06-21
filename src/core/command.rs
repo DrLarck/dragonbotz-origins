7b5bc594a27938257e44bf2445171ecfccebacd8
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-06-21 21:58:12 +0200

    Adds options method

--- src/core/command.rs
+++ src/core/command.rs
@@ -2,7 +2,10 @@
 // lib
     // serenity
 use serenity::async_trait;
-use serenity::builder::CreateEmbed;
+use serenity::builder::{
+    CreateEmbed,
+    CreateApplicationCommandOption,
+};
 use serenity::client::Context;
 
 
@@ -15,10 +18,13 @@ pub trait Command: Sync + Send {
     /// Returns the command's description
     fn description(self: &Self) -> String;
 
+    /// Returns the command's options
+    fn options(self: &Self) -> Option<Vec<CreateApplicationCommandOption>> { None }
+
     /// Returns the command's content
-    async fn content(self: &Self, context: &Context) -> Option<String> { None }
+    async fn content(self: &Self, _context: &Context) -> Option<String> { None }
 
     /// Returns the command's embed
-    async fn embed(self: &Self, context: &Context) -> Option<CreateEmbed> { None }
+    async fn embed(self: &Self, _context: &Context) -> Option<CreateEmbed> { None }
 
 }
