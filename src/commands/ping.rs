28ec1f9b187f2b9a5310742a6946a82b7beddbbe
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-06-27 19:39:20 +0200

    Updates ping command for test purpose

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,19 +1,30 @@
 
 // lib
+    // std
+use std::time;
+
     // serenity
 use serenity::async_trait;
 use serenity::builder::{
     CreateEmbed,
     CreateApplicationCommandOption,
+    CreateActionRow,
+    CreateButton,
 };
 use serenity::client::Context;
-use serenity::model::interactions::application_command::ApplicationCommandOptionType;
+use serenity::model::interactions::application_command::{
+    ApplicationCommandOptionType,
+    ApplicationCommandInteractionDataOption,
+    ApplicationCommandInteractionDataOptionValue,
+};
+use serenity::model::interactions::message_component::ButtonStyle;
 
 // core
 use crate::core::command::Command;
 
 // utils
 use crate::utils::embed::Embed;
+use crate::utils::other::Utils;
 
 
 pub struct PingCommand;
@@ -43,11 +54,33 @@ impl Command for PingCommand {
         Some(options)
     }
 
-    async fn content(&self, _: &Context) -> Option<String> {
-        Some("Pong ! Eheh".to_string())
+    async fn content(&self, 
+                     _: &Context, 
+                     options: &Vec<ApplicationCommandInteractionDataOption>) 
+        -> Option<String> {
+        
+        let mut user_name = String::new();
+        
+        // get name option
+        if let Some(name_option) = options.get(0) {
+
+            let name = Utils::option_value(name_option);
+            if let ApplicationCommandInteractionDataOptionValue::String(value) = name {
+                user_name = value;
+            } else {
+                return Some("error".to_string());
+            }
+
+        }
+
+        Some(format!("Pong ! Eheh: {}", user_name).to_string())
     }
 
-    async fn embed(&self, context: &Context) -> Option<CreateEmbed> {
+    async fn embed(&self, 
+                   context: &Context,
+                    _: &Vec<ApplicationCommandInteractionDataOption>) 
+        -> Option<CreateEmbed> {
+
         let mut embed = Embed::new(context);
 
         let stats = "♥️ Health: **666**/**666**
@@ -62,6 +95,43 @@ impl Command for PingCommand {
         embed.add_field("This is a field on the right".to_string(), "another value :)".to_string(), true);
 
         Some(embed.create())
+    }
+
+    async fn buttons(&self,
+                     _context: &Context,
+                     _options: &Vec<ApplicationCommandInteractionDataOption>)
+        -> Option<Vec<CreateButton>> {
+        
+        let mut buttons = Vec::<CreateButton>::new();
+
+        let mut button = CreateButton::default();
+        button.custom_id("sample_button_id");
+        button.label("This is a button");
+
+        let mut sec_button = CreateButton::default();
+        sec_button.custom_id("danger_button");
+        sec_button.label("Danger");
+        sec_button.style(ButtonStyle::Danger);
+
+        let mut link = CreateButton::default();
+        link.label("link");
+        link.url("https://google.com");
+        link.style(ButtonStyle::Link);
+
+        let mut sec = CreateButton::default();
+        sec.custom_id("sec");
+        sec.label("sec");
+        sec.style(ButtonStyle::Secondary);
+
+        buttons.push(button);
+        buttons.push(sec_button);
+        buttons.push(link);
+        buttons.push(sec);
+        Some(buttons)
+    }
+
+    async fn timeout(&self) -> Option<time::Duration> {
+        Some(time::Duration::from_secs(60))
     }
 
 }
