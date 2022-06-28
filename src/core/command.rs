26567c51a1cf750f6d9e29c10782d7b911593e1a
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-06-28 13:08:57 +0200

    Fixes author id fetching

--- src/core/command.rs
+++ src/core/command.rs
@@ -10,9 +10,14 @@ use serenity::builder::{
     CreateApplicationCommandOption,
     CreateActionRow,
     CreateButton,
+    CreateComponents,
 };
 
-use serenity::model::interactions::application_command::ApplicationCommandInteractionDataOption;
+use serenity::model::interactions::application_command::{
+    ApplicationCommandInteraction,
+    ApplicationCommandInteractionDataOption,
+};
+
 use serenity::client::Context;
 
 
@@ -28,6 +33,87 @@ pub trait Command: Sync + Send {
     /// Returns the command's options
     fn options(self: &Self) 
         -> Option<Vec<CreateApplicationCommandOption>> { None }
+
+    // Runs the command
+    async fn run(self: &Self, 
+                 context: &Context,
+                 command: &ApplicationCommandInteraction) {
+        // init
+        let channel = command.channel_id;
+        let command_options = &command.data.options;
+        
+        // find ther user's id of the caller
+        let author_id = command.user.id;
+        
+        // get command data
+        let mut message_content = String::new();
+        let mut has_content = false;
+        if let Some(content) = self.content(context, command_options).await {
+            message_content = content;
+            has_content = true;
+        }
+
+        let mut message_embed = Vec::<CreateEmbed>::new();
+        let mut has_embed = false;
+        if let Some(embed) = self.embed(context, command_options).await {
+            message_embed = vec![embed];
+            has_embed = true;
+        }
+
+        let mut message_action_row = CreateComponents::default();
+        let mut has_action_row = false;
+        if let Some(action_row) = self.action_row(context, command_options).await {
+            message_action_row.set_action_row(action_row);
+            has_action_row = true;
+        }
+
+        let response = channel.send_message(
+            &context.http,
+            |message| {
+                if has_content {
+                    message.content(message_content);
+                }
+                if has_embed {
+                    message.add_embeds(message_embed);
+                }
+                if has_action_row {
+                    message.set_components(message_action_row);
+                }
+
+                message
+            }
+        ).await;
+
+        if let Ok(message) = &response {
+            let mut limit_duration = time::Duration::from_secs(60);
+            if let Some(timeout) = self.timeout().await {
+                limit_duration = timeout;
+            }
+
+            let message_id = message.id;
+
+            // setup the interaction await
+            let component_interaction = message
+                .await_component_interaction(&context)
+                .author_id(author_id)
+                .message_id(message_id)
+                .collect_limit(1)
+                .timeout(limit_duration)
+                .await;
+
+            if let Some(interaction_response) = component_interaction {
+                if interaction_response.data.custom_id == "sample_button_id" {
+                    println!("blue");
+                }
+            } else {
+                return;  // timout
+            }
+        }
+
+        if let Err(error) = &response {
+            panic!("Error in Command::run: Unable to send message ({}).", error);
+        }
+    }
 
     /// Returns the command's content
     async fn content(self: &Self, 
